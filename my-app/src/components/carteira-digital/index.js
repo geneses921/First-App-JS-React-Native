2a71a60cd87628d3f57443a7bac19460361011b8
Geneses Geovane <geneses921@gmail.com>
2024-05-03 20:54:05 -0300

    primeiro commit do My App

---
+++ my-app/src/components/carteira-digital/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function CarteiraDigital() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ fontSize: 24, fontWeight: "bold"}}>Lista carteira digital</Text>
+        </View>
+    );
+}
